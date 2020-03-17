2b7cbd35a18ffaa225f73c29bd1dab3e2476c209
Tariel <tar.iel.reg@gmail.com>
2020-03-17 13:07:47 -0400

    README.md file created

---
+++ src/test/java/pro/tariel/jenkinstest/resource/DummyControllerTest.java
@@ -0,0 +1,25 @@
+package pro.tariel.jenkinstest.resource;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DummyControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getString() {
+    }
+
+    @Test
+    void getItem() {
+    }
+
+    @Test
+    void getResponse() {
+    }
+}
---
+++ src/test/java/pro/tariel/jenkinstest/util/TestUtils.java
@@ -0,0 +1,4 @@
+package pro.tariel.jenkinstest.util;
+
+public class TestUtils {
+}
