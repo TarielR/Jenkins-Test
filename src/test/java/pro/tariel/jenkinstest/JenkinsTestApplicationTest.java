036fe8c64d5a41c158cfed73a4c3eeb8a1bcf824
Tariel <tar.iel.reg@gmail.com>
2020-03-17 11:52:23 -0400

    change class name

--- src/test/java/pro/tariel/jenkinstest/JenkinsTestApplicationTest.java
+++ src/test/java/pro/tariel/jenkinstest/JenkinsTestApplicationTest.java
@@ -1,13 +1,11 @@
 package pro.tariel.jenkinstest;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
-public class JenkinsTestApplicationTests {
+public class JenkinsTestApplicationTest {
 
     @Test
     public void contextLoads() {
