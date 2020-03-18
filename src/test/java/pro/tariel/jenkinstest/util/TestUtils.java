b05b5a848b7a7743c65b9a1aed9968339f2f0e14
Tariel <tar.iel.reg@gmail.com>
2020-03-17 20:56:57 -0400

    TestUtils class created

---
+++ src/test/java/pro/tariel/jenkinstest/util/TestUtils.java
@@ -0,0 +1,32 @@
+package pro.tariel.jenkinstest.util;
+
+import pro.tariel.jenkinstest.domain.Asset;
+import pro.tariel.jenkinstest.domain.Item;
+
+import java.time.LocalDateTime;
+
+public class TestUtils {
+    private static Asset retrieveAssetWithoutNull() {
+        return new Asset("40", "30", "20","3", "7");
+    }
+
+    private static Asset retrieveAssetWithOneNull() {
+        return new Asset("40", "30", "20",null, "10");
+    }
+
+    private static Asset retrieveAssetWithTwoNulls() {
+        return new Asset("40", "30", null,null, "30");
+    }
+
+    private static Asset retrieveAssetWithThreeNulls() {
+        return new Asset("40", null, "60",null, null);
+    }
+
+    private static Asset retrieveAssetWithFourNulls() {
+        return new Asset(null, "100", null,null, null);
+    }
+
+    public static Item retrieveItem() {
+        return new Item("First item", LocalDateTime.now(), retrieveAssetWithoutNull());
+    }
+}
