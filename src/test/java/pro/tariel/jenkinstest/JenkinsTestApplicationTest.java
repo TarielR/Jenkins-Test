f45bd7e9257f771d28d56da703c3ec77d7a74d83
Tariel <tar.iel.reg@gmail.com>
2020-03-17 21:37:11 -0400

    Some test for JenkinsTestApplication class

--- src/test/java/pro/tariel/jenkinstest/JenkinsTestApplicationTest.java
+++ src/test/java/pro/tariel/jenkinstest/JenkinsTestApplicationTest.java
@@ -1,14 +1,28 @@
 package pro.tariel.jenkinstest;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
 
-//@RunWith(SpringRunner.class)
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class JenkinsTestApplicationTest {
 
+    @Autowired
+    protected WebApplicationContext wac;
+
+    @Autowired
+    protected JenkinsTestApplication cxt;
+
     @Test
     public void contextLoads() {
-    }
+        assertNotNull(wac.getServletContext());
+        assertNotNull(cxt);
 
+    }
 }
